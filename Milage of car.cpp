48d1524cfe4907fdc228b515c198bb996083a752
Muhammad Bilal Gulzar <79194902+Bilal-731@users.noreply.github.com>
2021-02-24 12:41:55 +0500

    Add files via upload

---
+++ Milage of car.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+float distance(float &petrol)
+
+{
+	petrol*mileage;
+	return main ();
+	
+}
+int main()
+{
+float petrol,mileage;
+
+cout<<"Enter petrol: ";
+
+cin>>petrol;
+
+mileage=5.3;
+
+cout<<"Distance car can travel is: ";
+
+cout<<distance(float &petrol);
+}
+
